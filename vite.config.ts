3c72a33686723e9c692cae2a928458549bfb9f73
christian <christian.carvalho.adv@gmail.com>
2026-01-21 20:18:18 -0300

    Correção de dependencias e config Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@react-router/cloudflare/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    reactRouter(), // Sem argumentos
+    cloudflare(), // <--- Essa é a peça chave para funcionar na Cloudflare
+    reactRouter(),
     tsconfigPaths(),
   ],
 });
